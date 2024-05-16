60a208fba0f6db3949d154f75cbd5120b6d63f3b
Mary Shtaida <mshtaidabel@gmail.com>
2024-05-16 07:15:35 +0300

    add kursach

---
+++ EWT/Курсач эвт/js/script.js
@@ -0,0 +1,52 @@
+//карусель
+
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+
+let pictures =  ['../images/swiper-1.jpeg', '../images/swiper-2.jpeg', '../images/swiper-3.jpeg', '../images/swiper-4.jpeg', '../images/swiper-5.jpeg', '../images/swiper-6.jpeg'];
+
+img.src = pictures[0];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
+
+
+
+
+//тема
+let switchMode = document.getElementById("btn");
+let theme = document.getElementById('theme');
+switchMode.onclick = function(){
+    let theme = document.getElementById('theme');
+
+    if (theme.getAttribute("href") == "/style/style_light.css"){
+        theme.href = "/style/style.css";
+
+    } else{
+        theme.href = "/style/style_light.css";
+
+    }
+}
